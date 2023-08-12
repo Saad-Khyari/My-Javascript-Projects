c05d8f6ebb01c884da31d864d680e79fbc29b58f
Saad Khyari <124291463+Saad-Khyari@users.noreply.github.com>
2023-08-12 12:23:14 +0100

    Update notepad.js

--- Notes App/notepad.js
+++ Notes App/notepad.js
@@ -1,17 +1,17 @@
 const noteCon= document.querySelector(".notes-container");
 const createBtn= document.querySelector(".btn");
 let notes= document.querySelectorAll(".input-box");
-
+//this function allow user to display the notes after saving it
 function showNotes(){
     noteCon.innerHTML=localStorage.getItem("notes");
 }
 showNotes();
-
+//this function allow user to update notes
 function updateStorage(){
     localStorage.setItem("notes",noteCon.innerHTML);
 }
 
-
+//will clicking on the button this event allow to create a paragraph which is content editable
 createBtn.addEventListener("click", ()=>{
     let inputBox= document.createElement("p");
     let img = document.createElement("img");
@@ -21,6 +21,7 @@ createBtn.addEventListener("click", ()=>{
     noteCon.appendChild(inputBox).appendChild(img);
 
 })
+//this event allow us to delete the content by clicking on the delete icon inside the paragraph box
 noteCon.addEventListener("click", function(e){
     if (e.target.tagName === "IMG") {
         e.target.parentElement.remove();
